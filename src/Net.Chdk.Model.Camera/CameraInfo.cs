300c2c432fe1ca238b7deeb713cb78424ec8bd06
Dmitry Shechtman <damnian@gmail.com>
2017-04-24 17:44:41 +0300

    Change Version to System.Version

--- src/Net.Chdk.Model.Camera/CameraInfo.cs
+++ src/Net.Chdk.Model.Camera/CameraInfo.cs
@@ -1,8 +1,10 @@
-﻿namespace Net.Chdk.Model.Camera
+﻿using System;
+
+namespace Net.Chdk.Model.Camera
 {
     public sealed class CameraInfo
     {
-        public string Version { get; set; }
+        public Version Version { get; set; }
         public BaseInfo Base { get; set; }
         public CanonInfo Canon { get; set; }
     }
