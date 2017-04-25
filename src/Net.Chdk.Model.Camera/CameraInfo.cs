942a44db44c5fc0701237b462d29f31aa31a9588
Dmitry Shechtman <damnian@gmail.com>
2017-04-25 17:20:43 +0300

    Fix Version serialization

--- src/Net.Chdk.Model.Camera/CameraInfo.cs
+++ src/Net.Chdk.Model.Camera/CameraInfo.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 
 namespace Net.Chdk.Model.Camera
 {
     public sealed class CameraInfo
     {
+        [JsonConverter(typeof(VersionConverter))]
         public Version Version { get; set; }
+
         public BaseInfo Base { get; set; }
         public CanonInfo Canon { get; set; }
     }
