dee318f88503dba4aa42c935556b718eaadb525e
Dmitry Shechtman <damnian@gmail.com>
2017-05-01 23:54:33 +0300

    Remove VersionConverter

--- src/Net.Chdk.Model.Camera/CameraInfo.cs
+++ src/Net.Chdk.Model.Camera/CameraInfo.cs
@@ -1,14 +1,10 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
-using System;
+﻿using System;
 
 namespace Net.Chdk.Model.Camera
 {
     public sealed class CameraInfo
     {
-        [JsonConverter(typeof(VersionConverter))]
         public Version Version { get; set; }
-
         public BaseInfo Base { get; set; }
         public CanonInfo Canon { get; set; }
     }
