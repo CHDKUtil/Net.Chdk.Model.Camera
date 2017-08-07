5327c4b0d9684513783708bc35a09414f261728a
Dmitry Shechtman <damnian@gmail.com>
2017-08-07 19:15:37 +0300

    Remove METADATA

--- src/Net.Chdk.Model.Camera/CameraInfo.cs
+++ src/Net.Chdk.Model.Camera/CameraInfo.cs
@@ -2,9 +2,6 @@
 {
     public sealed class CameraInfo
     {
-#if METADATA
-        public System.Version Version { get; set; }
-#endif
         public BaseInfo Base { get; set; }
         public CanonInfo Canon { get; set; }
     }
